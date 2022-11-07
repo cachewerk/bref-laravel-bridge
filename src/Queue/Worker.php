e5800b1a4a25c453f5638ebc366c4e8626bc42dc
Till Krüss <tillkruss@users.noreply.github.com>
2022-11-06 17:02:59 -0800

    cleanup

--- src/Queue/Worker.php
+++ src/Queue/Worker.php
@@ -3,11 +3,19 @@
 namespace CacheWerk\BrefLaravelBridge\Queue;
 
 use Illuminate\Contracts\Queue\Job;
-use Illuminate\Queue\Worker as LaravelWorker;
 use Illuminate\Queue\WorkerOptions;
+use Illuminate\Queue\Worker as LaravelWorker;
 
 class Worker extends LaravelWorker
 {
+    /**
+     * Creates a new SQS queue handler instance.
+     *
+     * @param  \Illuminate\Contracts\Queue\Job  $job
+     * @param  string  $connectionName
+     * @param  \Illuminate\Queue\WorkerOptions  $options
+     * @return void
+     */
     public function runSqsJob(Job $job, string $connectionName, WorkerOptions $options): void
     {
         $this->runJob($job, $connectionName, $options);
