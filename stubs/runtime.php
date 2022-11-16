090065687aee0f8f0d22e962caafba1c27d4a199
Till Krüss <tillkruss@users.noreply.github.com>
2022-11-16 07:56:48 -0800

    Update runtime.php

--- stubs/runtime.php
+++ stubs/runtime.php
@@ -68,7 +68,7 @@ if ($runtime === 'octane') {
         (bool) ($_ENV['OCTANE_PERSIST_DATABASE_SESSIONS'] ?? false)
     );
 
-    return new OctaneHandler();
+    return new OctaneHandler;
 }
 
 return new HttpHandler(
