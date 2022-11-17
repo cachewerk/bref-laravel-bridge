c28bc5a2af77c7026a708a69cfb32de49fda9284
George Boot <884482+georgeboot@users.noreply.github.com>
2022-11-17 15:41:46 +0100

    Don't use facades

--- src/Secrets.php
+++ src/Secrets.php
@@ -3,6 +3,7 @@
 namespace CacheWerk\BrefLaravelBridge;
 
 use Aws\Ssm\SsmClient;
+use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Http\Client\Pool;
 use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
@@ -38,18 +39,20 @@ class Secrets
 
     protected static function resolveUsingLambdaExtension(string $path, array $parameters): array
     {
-        $responses = Http::pool(
-            fn (Pool $pool) => collect($parameters)->map(
-                fn (string $secret) => $pool->as($secret)->withHeaders([
-                    'X-Aws-Parameters-Secrets-Token' => env('AWS_SESSION_TOKEN'),
-                ])->get('http://localhost:2773/systemsmanager/parameters/get', [
-                    'name' => $path . $secret,
-                    'withDecryption' => 'true',
-                ])
-            )
-        );
+        $httpFactory = new \Illuminate\Http\Client\Factory();
+        $httpPool = new \Illuminate\Http\Client\Pool($httpFactory);
 
-        return collect($responses)
+        foreach ($parameters as $secret) {
+            $httpPool->as($secret)->withHeaders([
+                'X-Aws-Parameters-Secrets-Token' => env('AWS_SESSION_TOKEN'),
+            ])->get('http://localhost:2773/systemsmanager/parameters/get', [
+                'name' => $path . $secret,
+                'withDecryption' => 'true',
+            ]);
+        }
+
+        return collect($httpPool->getRequests())
+            ->map(fn (PendingRequest $request) => $request->getPromise()->wait())
             ->filter(fn (Response $response) => $response->ok())
             ->map(fn (Response $response) => $response->json('Parameter.Value'))
             ->toArray();
