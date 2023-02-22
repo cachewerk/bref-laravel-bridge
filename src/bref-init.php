de2c62718919dfe028eea1525356b76b83f8ffc1
Till Krüss <tillkruss@users.noreply.github.com>
2023-02-22 07:26:58 -0800

    flatten

--- src/bref-init.php
+++ src/bref-init.php
@@ -1,7 +1,7 @@
 <?php
-declare(strict_types=1);
 
 use Bref\Bref;
+
 use CacheWerk\BrefLaravelBridge\HandlerResolver;
 use CacheWerk\BrefLaravelBridge\StorageDirectories;
 
@@ -13,22 +13,23 @@ Bref::beforeStartup(static function () {
     StorageDirectories::create();
 
     $defaultConfigCachePath = $_SERVER['LAMBDA_TASK_ROOT'] . '/bootstrap/cache/config.php';
-    $laravelWasDeployedWithCachedConfig = file_exists($defaultConfigCachePath);
 
-    if (! $laravelWasDeployedWithCachedConfig) {
-        // Move the location of the config cache to `/tmp` (because it is writable)
-        $newConfigCachePath = StorageDirectories::Path . '/bootstrap/cache/config.php';
+    if (file_exists($defaultConfigCachePath)) {
+        return;
+    }
 
-        // Automatically caches the configuration if it does not exist (only once)
-        if (! file_exists($newConfigCachePath)) {
-            $_SERVER['APP_CONFIG_CACHE'] = $_ENV['APP_CONFIG_CACHE'] = $newConfigCachePath;
-            putenv('APP_CONFIG_CACHE=' . $newConfigCachePath);
+    // Move the location of the config cache to `/tmp` (because it is writable)
+    $newConfigCachePath = StorageDirectories::Path . '/bootstrap/cache/config.php';
 
-            fwrite(STDERR, "Running 'php artisan config:cache' to cache the Laravel configuration\n");
+    // Automatically caches the configuration if it does not exist (only once)
+    if (! file_exists($newConfigCachePath)) {
+        $_SERVER['APP_CONFIG_CACHE'] = $_ENV['APP_CONFIG_CACHE'] = $newConfigCachePath;
+        putenv('APP_CONFIG_CACHE=' . $newConfigCachePath);
 
-            // 1>&2 redirects the output to STDERR to avoid messing up HTTP responses with FPM
-            passthru('php artisan config:cache 1>&2');
-        }
+        fwrite(STDERR, "Running 'php artisan config:cache' to cache the Laravel configuration\n");
+
+        // 1>&2 redirects the output to STDERR to avoid messing up HTTP responses with FPM
+        passthru('php artisan config:cache 1>&2');
     }
 });
 
